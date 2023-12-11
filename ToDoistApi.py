59f8672d7275d7c1be47393310620cfd8e2525c5
KayMaN777 <108622492+KayMaN777@users.noreply.github.com>
2023-12-11 20:49:55 +0300

    Update ToDoistApi.py

--- ToDoistApi.py
+++ ToDoistApi.py
@@ -24,8 +24,7 @@ class MyApi:
     def AddProject(self, name, parent = None, style = "list", color = "charcoal"):
         try:
             parent_id = self.GetProjectId(parent)
-            project = self.api.add_project(name = name, parent_id = parent_id, view_style = style, color = color)
-            return project.id
+            self.api.add_project(name = name, parent_id = parent_id, view_style = style, color = color)
         except Exception as e:
             raise e
     
@@ -41,8 +40,7 @@ class MyApi:
     def RenameProject(self, name, new_name):
         try:
             project_id = self.GetProjectId(name)
-            project = self.api.update_project(project_id = project_id, name = new_name)
-            return project.id
+            self.api.update_project(project_id = project_id, name = new_name)
         except Exception as e:
             raise e
 
@@ -60,8 +58,7 @@ class MyApi:
     def AddTask(self, content, project = "Inbox", description = None, date = None, priority = 1):
         try:
             project_id = self.GetProjectId(project)
-            task = self.api.add_task(content=content, project_id = project_id, description = description, due_date = date, priority = priority)
-            return task
+            self.api.add_task(content=content, project_id = project_id, description = description, due_date = date, priority = priority)
         except Exception as e:
             raise e
 
