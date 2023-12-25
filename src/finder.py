4ea58afa0866c9a4b0914510e5fd143d9bc117ef
Андрей <konderlip@gmail.com>
2023-12-25 10:21:11 +0300

    Update

--- src/finder.py
+++ src/finder.py
@@ -1,4 +1,3 @@
-from dataclasses import dataclass
 from typing import List, Optional, Tuple
 
 import numpy as np
@@ -8,20 +7,12 @@ from thefuzz.fuzz import partial_token_set_ratio
 from transformers import AutoTokenizer, AutoModel
 import torch
 
-from nltk.tokenize import RegexpTokenizer
 import re
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 model_name = "cointegrated/LaBSE-en-ru"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModel.from_pretrained(model_name).to(device)
-
-name_pattern = r"\"[^\"]+\""
-name_tokenizer = RegexpTokenizer(name_pattern)
-date_pattern = r"\d{2}\.\d{2}\.\d{4}|\d{2}\.\d{2}\.\d{2}|сегодня|завтра"
-date_tokenizer = RegexpTokenizer(date_pattern)
-time_pattern = r"\d{1,2}:\d{2}"
-time_tokenizer = RegexpTokenizer(time_pattern)
 
 
 def find_closest(query: str, candidates: List[str]) -> Optional[Tuple[int, int]]:
@@ -69,18 +60,6 @@ def get_embeds(sentences: List[str]) -> np.ndarray:
     return np.array([embedding.numpy() for embedding in embeddings])
 
 
-@dataclass
-class Attrs:
-    """
-    Метаданные, включая тип запроса
-    """
-
-    type_idx: int
-    nearest: List[Optional[Tuple[int, int]]]
-    name: Optional[str]
-    due_string: Optional[str]
-
-
 class Finder:
     """
     Класс для поиска ближайшего типа запроса и получения его атрибутов
@@ -93,10 +72,11 @@ class Finder:
 
         self._type_embeds = get_embeds([clear_text(t) for t in types])
 
-    def get_attrs(self, query: str, candidates: List[List[str]]) -> Attrs:
+    def get(self, query: str, candidates: List[List[str]]) -> Tuple[int, List[Optional[Tuple[int, int]]]]:
         """
         :param query: Запрос
-        :return: Атрибуты и ближайший тип
+        :param candidates: Список кандидатов
+        :return: Ближайший тип и лучшие кандидаты
         """
 
         nearest = []
@@ -108,15 +88,4 @@ class Finder:
         query_embed = get_embeds([query])
         type_idx = np.argmax(query_embed @ self._type_embeds.T, axis=1)[0]
 
-        name = name_tokenizer.tokenize(query)
-        name = name[0][1: -1].strip() if len(name) == 1 else None
-
-        date = date_tokenizer.tokenize(query)
-        date = date[0] if len(date) == 1 else ""
-        time = time_tokenizer.tokenize(query)
-        time = time[0] if len(time) == 1 else ""
-        due_string = date + ' ' + time
-        if due_string == ' ':
-            due_string = None
-
-        return Attrs(type_idx, nearest, name, due_string)
+        return type_idx, nearest
--- src/handlers/smart.py
+++ src/handlers/smart.py
@@ -1,17 +1,57 @@
+from typing import List, Optional, Tuple
+
 import asyncio
 
 from aiogram import Router, F
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, CallbackQuery
 
+from nltk import RegexpTokenizer
+
 from src import api, kb, states
 
-from src.finder import Attrs, Finder
+from src.finder import Finder, clear_text
 from src.handlers.base import funcs, choose_query
 
 router = Router()
 
+name_tokenizer = RegexpTokenizer(r"\"[^\"]+\"")
+date_tokenizer = RegexpTokenizer(r"\d{2}\.\d{2}\.\d{4}|\d{2}\.\d{2}\.\d{2}")
+time_tokenizer = RegexpTokenizer(r"\d{1,2}:\d{2}")
 finder = Finder(api.types)
+
+
+def add_name(query: str, data: dict) -> None:
+    name = None
+    if data["type_idx"] == 0 or data["type_idx"] == 3:
+        res = name_tokenizer.tokenize(query)
+        if len(res) == 1:
+            name = res[0][1: -1].strip()
+    if data["type_idx"] == 0 and name is not None:
+        data["name"] = name
+    if 3 <= data["type_idx"] <= 5 and "name" not in data:
+        data["name"] = "Inbox"
+    if data["type_idx"] == 3 and name is not None:
+        data["content"] = name
+        if data["name"] == name:
+            data["name"] = "Inbox"
+
+
+def add_due_string(query: str, data: dict) -> None:
+    query = query.lower()
+    date = date_tokenizer.tokenize(query)
+    date = date[0] if len(date) == 1 else ""
+    if date == "":
+        for v in ("сегодня", "today", "завтра", "tomorrow", "вчера", "yesterday"):
+            if v in query:
+                date = v
+                break
+    time = time_tokenizer.tokenize(query)
+    time = time[0] if len(time) == 1 else ""
+    due_string = date + " " + time
+    due_string = due_string.strip()
+    if due_string != "":
+        data["due_string"] = due_string
 
 
 @router.message(states.Menu.choose_query)
@@ -26,25 +66,21 @@ async def get_custom_query(msg: Message, state: FSMContext):
         contents.extend([task.content for task in tasks])
 
     query = msg.text.strip()
-    res = await asyncio.gather(asyncio.to_thread(finder.get_attrs, query, [names, contents]))
-    attrs: Attrs = res[0]
-    print(query, api.types[attrs.type_idx], attrs)  # TODO
+    res = await asyncio.gather(asyncio.to_thread(finder.get, query, [names, contents]))
+    type_idx: int = res[0][0]
+    nearest: List[Optional[Tuple[int, int]]] = res[0][1]
 
-    data["type_idx"] = attrs.type_idx
-    # if attrs.due_string is not None:
-    #     data["due_string"] = attrs.due_string
+    data["type_idx"] = type_idx
 
-    if attrs.nearest[0] is not None and attrs.nearest[0][1] >= 50:
-        data["name"] = names[attrs.nearest[0][0]]
-    if attrs.nearest[1] is not None and attrs.nearest[1][1] >= 50:
-        data["content"] = contents[attrs.nearest[1][0]]
+    if nearest[0] is not None and nearest[0][1] >= 50:
+        data["name"] = names[nearest[0][0]]
+    if nearest[1] is not None and nearest[1][1] >= 50:
+        data["content"] = contents[nearest[1][0]]
 
-    # if attrs.name is not None:
-    #     if "задач" in api.types[attrs.type_idx].lower():
-    #         data["name"] = "Inbox"
-    #         data["content"] = attrs.name
-    #     else:
-    #         data["name"] = attrs.name
+    query = clear_text(query)
+
+    await asyncio.gather(asyncio.to_thread(add_name, query, data))
+    await asyncio.gather(asyncio.to_thread(add_due_string, query, data))
 
     await state.set_data(data)
     await state.set_state(states.CustomQuery.state)
@@ -74,10 +110,12 @@ async def drop_attrs(msg: Message, state: FSMContext):
             data.pop("type_idx")
         case "имя проекта":
             data.pop("name")
+        case "имя задачи":
+            data.pop("content")
         case "дедлайн":
             data.pop("due_string")
         case _:
-            await incorrect(msg, state)
+            await incorrect(msg)
             return
 
     await state.set_data(data)
